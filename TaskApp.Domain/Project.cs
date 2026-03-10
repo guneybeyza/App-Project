13b8bd4a973fe3e7cf2ab3242e57fd421c05ba52
TalhaElma67 <ahmettalhaelma67@gmail.com>
2026-03-10 15:01:19 +0300

    Project ve Task sınıfları oluşturma.

---
+++ TaskApp.Domain/Project.cs
@@ -0,0 +1,10 @@
+namespace TaskApp.Domain
+{
+    internal class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int UserId { get; set; }
+    }
+}
